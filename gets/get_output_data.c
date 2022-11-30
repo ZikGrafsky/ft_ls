6a31b1f6060ba7ecf3ad9c8ff5a568aeb3c2e045
ZikGrafsky <silentzzephyr@gmail.com>
2022-11-30 05:37:02 +0300

    remove leaks

--- gets/get_output_data.c
+++ gets/get_output_data.c
@@ -13,8 +13,11 @@ char **get_output_data(char **args, t_options *out_options){
         stat(args[i], &fileStat);
         if (S_ISDIR(fileStat.st_mode)) {
             opened = opendir(args[i]);
-            if (duarrlen(args) > 1)
-                output_strings = stradd(ft_strjoin(args[i], ":"), output_strings);
+            if (duarrlen(args) > 1) {
+                char *tmp = ft_strjoin(args[i], ":");
+                output_strings = stradd(tmp, output_strings);
+                free(tmp);
+            }
             while ((tmp = readdir(opened)) != NULL)
                 if (tmp->d_name[0] != '.' || out_options->a_flag) {
                     if (out_options->l_flag) {
